2fedbc40fb6ea80decdab98a7e1e2bd199429647
BorisArgirov <121773073+BorisArgirov@users.noreply.github.com>
2023-10-13 19:47:16 +0200

    Update smallest_of_three_nunmbers.py

--- python_fundamentals/functions/exercise/smallest_of_three_nunmbers.py
+++ python_fundamentals/functions/exercise/smallest_of_three_nunmbers.py
@@ -1,1 +1,8 @@
+def smallest_number(num_list:list):
+    return min(num_list)
 
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+smallest_num = min([num1, num2, num3])
+print(smallest_num)
