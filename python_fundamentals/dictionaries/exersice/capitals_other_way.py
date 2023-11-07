3f8b8b134a725fb6ab5062a7e3e0147514eb229c
BorisArgirov <121773073+BorisArgirov@users.noreply.github.com>
2023-11-07 21:19:22 +0100

    Add files via upload

---
+++ python_fundamentals/dictionaries/exersice/capitals_other_way.py
@@ -0,0 +1,7 @@
+countries = input().split(', ')
+capitals = input().split(', ')
+
+final_result = dict(zip(countries, capitals))
+
+for key, value in final_result.items():
+    print(f'{key} -> {value}')
