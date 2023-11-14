e4c11d4f1a30cdc76b13d0574a43ad0ce6d304aa
BorisArgirov <121773073+BorisArgirov@users.noreply.github.com>
2023-11-14 22:24:08 +0100

    Add files via upload

---
+++ python_fundamentals/dictionaries/odd_occurrences.py
@@ -0,0 +1,16 @@
+words_list = input().split()
+
+dictionary = {}
+
+for word in words_list:
+    word = word.lower()
+    if word not in dictionary:
+        dictionary[word] = 0
+    dictionary[word] += 1
+
+for key, value in dictionary.items():
+    if value % 2 != 0:
+        print(key, end=' ')
+
+
+
